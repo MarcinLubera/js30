f4fb594e3f9c66ced2e5377ef34ad33a2e574dce
Marcin <lubera.marcin@gmail.com>
2019-11-20 21:38:23 +0100

    finish lesson 5 with bugs

---
+++ lesson5/script.js
@@ -0,0 +1,16 @@
+
+const panels = document.querySelectorAll('.panel');
+
+function toggleOpen() {
+  this.classlist.toggle('open');
+}
+
+function toggleActive(e) {
+  console.log(e.propertyName);
+if (e.propertyName.includes('flex')){
+  this.classlist.toggle('open-active');
+}
+}
+
+panels.forEach(panel => panel.addEventListener('click', toggleOpen));
+panels.forEach(panel => panel.addEventListener('transitionend', toggleActive));
